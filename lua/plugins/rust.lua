7b4bdcf2634c409a86a6d4ac5cd5006bcea8d6bd
Matthes Bär <Matthes.baer@yahoo.de>
2025-04-29 20:17:45 +0200

    chore: updated plugins & made rust plugins "lazy"

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,4 +1,4 @@
 return {
-  { "simrat39/rust-tools.nvim", config = true }, -- Better Rust support
-  { "saecki/crates.nvim", event = "BufRead Cargo.toml", config = true },
+  { "simrat39/rust-tools.nvim", ft = { "rust" }, config = true }, -- Load for Rust files
+  { "saecki/crates.nvim", event = { "BufRead Cargo.toml" }, config = true }, -- Load when Cargo.toml is read
 }
